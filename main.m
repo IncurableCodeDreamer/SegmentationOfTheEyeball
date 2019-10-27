6c303428cac173dd6691f83db562e0b0d5a883ad
IncurableCodeDreamer <kla.96.kn@gmail.com>
2019-10-27 22:35:33 +0100

    Add export left and right photo & first version of adding dots.

--- main.m
+++ main.m
@@ -19,11 +19,8 @@ function varargout = main(varargin)
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
-
 % Edit the above text to modify the response to help main
-
 % Last Modified by GUIDE v2.5 26-Oct-2019 18:21:14
-
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -35,14 +32,12 @@ gui_State = struct('gui_Name',       mfilename, ...
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
-
 if nargout
     [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
 else
     gui_mainfcn(gui_State, varargin{:});
 end
 % End initialization code - DO NOT EDIT
-
 
 % --- Executes just before main is made visible.
 function main_OpeningFcn(hObject, eventdata, handles, varargin)
@@ -57,10 +52,8 @@ handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
-
 % UIWAIT makes main wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = main_OutputFcn(hObject, eventdata, handles) 
@@ -71,14 +64,21 @@ function varargout = main_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+clc;
+clear all;
 
-
-% --- Executes on button press in dotsStart.
 function dotsStart_Callback(hObject, eventdata, handles)
-% hObject    handle to dotsStart (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
+numPointsClicked = 0;
+while true
+  numPointsClicked = numPointsClicked + 1
+  [x(numPointsClicked), y(numPointsClicked)] = ginput(1);
+   hold(handles.photoLeft,'on');
+  plot(x(numPointsClicked), y(numPointsClicked), 'yo', 'MarkerSize', 15);
+%   if button == 3
+    % Exit loop if
+%     break;
+%   end
+end
 
 % --- Executes on button press in dotSave.
 function dotSave_Callback(hObject, eventdata, handles)
@@ -86,13 +86,11 @@ function dotSave_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-
 % --- Executes on button press in linesStart.
 function linesStart_Callback(hObject, eventdata, handles)
 % hObject    handle to linesStart (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 
 % --- Executes on button press in linesSave.
 function linesSave_Callback(hObject, eventdata, handles)
@@ -100,13 +98,11 @@ function linesSave_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-
 % --- Executes on button press in effusionStart.
 function effusionStart_Callback(hObject, eventdata, handles)
 % hObject    handle to effusionStart (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 
 % --- Executes on button press in effusionSave.
 function effusionSave_Callback(hObject, eventdata, handles)
@@ -114,43 +110,33 @@ function effusionSave_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-
 % --- Executes on button press in show.
 function show_Callback(hObject, eventdata, handles)
 % hObject    handle to show (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-
 % --- Executes on button press in dotsCheckbox.
 function dotsCheckbox_Callback(hObject, eventdata, handles)
 % hObject    handle to dotsCheckbox (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Hint: get(hObject,'Value') returns toggle state of dotsCheckbox
-
 
 % --- Executes on button press in linesCheckbox.
 function linesCheckbox_Callback(hObject, eventdata, handles)
 % hObject    handle to linesCheckbox (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Hint: get(hObject,'Value') returns toggle state of linesCheckbox
-
 
 % --- Executes on button press in effusionCheckbox.
 function effusionCheckbox_Callback(hObject, eventdata, handles)
 % hObject    handle to effusionCheckbox (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Hint: get(hObject,'Value') returns toggle state of effusionCheckbox
 
-
-
-% --- Executes on button press in selectPhotoLeft.
 function selectPhotoLeft_Callback(hObject, eventdata, handles)
 global leftPhoto; 
 global leftPhotoSize;
@@ -167,16 +153,11 @@ resizePos = get(handles.photoLeft,'Position');
 leftPhoto= imresize(leftPhoto, [resizePos(3) resizePos(3)]);
 axes(handles.photoLeft);
 imshow(leftPhoto);
-set(handles.leftPhoto,'Units','normalized');
 
-% --- Executes on button press in exportPhotoLeft.
 function exportPhotoLeft_Callback(hObject, eventdata, handles)
-% hObject    handle to exportPhotoLeft (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+Image = getframe(handles.photoLeft);
+imwrite(Image.cdata, 'mask_image_L.jpg');
 
-
-% --- Executes on button press in selectPhotoRight.
 function selectPhotoRight_Callback(hObject, eventdata, handles)
 global rightPhoto; 
 global rightPhotoSize;
@@ -193,11 +174,7 @@ resizePos = get(handles.photoRight,'Position');
 rightPhoto= imresize(rightPhoto, [resizePos(3) resizePos(3)]);
 axes(handles.photoRight);
 imshow(rightPhoto);
-set(handles.rightPhoto,'Units','normalized');
 
-
-% --- Executes on button press in exportPhotoRight.
 function exportPhotoRight_Callback(hObject, eventdata, handles)
-% hObject    handle to exportPhotoRight (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+Image = getframe(handles.photoRight);
+imwrite(Image.cdata, 'mask_image_R.jpg');
